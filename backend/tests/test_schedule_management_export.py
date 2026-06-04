3bf8dba2b0fa829cf70270681781b400b365d017
VNightingale69 <125214935+VNightingale69@users.noreply.github.com>
2026-06-04 10:13:46 -0500

    Align schedule export CSV with spreadsheet format

---
+++ backend/tests/test_schedule_management_export.py
@@ -0,0 +1,49 @@
+import unittest
+from datetime import date, time
+from types import SimpleNamespace
+
+from app.routes.api import (
+    _format_schedule_export_date,
+    _format_schedule_export_status,
+    _format_schedule_export_time,
+    _schedule_export_row_values,
+)
+
+
+class ScheduleManagementExportTest(unittest.TestCase):
+    def test_export_formats_match_spreadsheet_columns(self):
+        self.assertEqual(_format_schedule_export_date(date(2026, 8, 9)), '8/9/2026')
+        self.assertEqual(_format_schedule_export_time(time(9, 0)), '9:00')
+        self.assertEqual(_format_schedule_export_time(time(13, 5)), '13:05')
+        self.assertEqual(_format_schedule_export_status('scheduled'), 'SCHEDULED')
+
+    def test_export_row_uses_compact_schedule_shape(self):
+        values = _schedule_export_row_values(
+            SimpleNamespace(game_date=date(2026, 8, 9), kickoff_time=time(9, 0)),
+            SimpleNamespace(field_type='small'),
+            SimpleNamespace(field_name='Wave 1 ONE_MEDIUM_TWO_SMALL Small Field 2'),
+            SimpleNamespace(name='Westosha Stadium'),
+            SimpleNamespace(name='Westosha Coed 2-3 Maroon'),
+            SimpleNamespace(name='LCS Coed 2-3 Red'),
+            SimpleNamespace(division_group='Coed', name='2-3'),
+            SimpleNamespace(code='scheduled'),
+        )
+
+        self.assertEqual(
+            values,
+            [
+                '8/9/2026',
+                '9:00',
+                'coed_2_3',
+                'Westosha Coed 2-3 Maroon',
+                'LCS Coed 2-3 Red',
+                'Westosha Stadium',
+                'Wave 1 ONE_MEDIUM_TWO_SMALL Small Field 2',
+                'SMALL',
+                'SCHEDULED',
+            ],
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
